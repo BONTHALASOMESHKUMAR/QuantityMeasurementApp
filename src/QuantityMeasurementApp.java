1c5d49ded3c1485905e614b2d89fa2bbb1f68b31
BONTHALASOMESHKUMAR <someshkumarbonthala@gmail.com>
2026-04-15 10:18:09 +0530

    Fix package structure

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -2,7 +2,7 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-
+    // ===== FEET CLASS (same as UC1) =====
     public static class Feet {
         private final double value;
 
@@ -12,24 +12,53 @@ public class QuantityMeasurementApp {
 
         @Override
         public boolean equals(Object obj) {
-
             if (this == obj) return true;
-
-
             if (obj == null) return false;
-
             if (getClass() != obj.getClass()) return false;
-
 
             Feet other = (Feet) obj;
             return Double.compare(this.value, other.value) == 0;
         }
     }
 
-    public static void main(String[] args) {
+    // ===== INCHES CLASS (NEW IN UC2) =====
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // ===== FEET DEMO =====
+    public static void demonstrateFeetEquality() {
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
 
-        System.out.println(f1.equals(f2));
+        System.out.println("Feet equal: " + f1.equals(f2));
+    }
+
+    // ===== INCHES DEMO =====
+    public static void demonstrateInchesEquality() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+
+        System.out.println("Inches equal: " + i1.equals(i2));
+    }
+
+    // ===== MAIN =====
+    public static void main(String[] args) {
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
     }
 }
